7840aec5f8343618fe86b40868c7b9e63071fb7a
jwollaars <jordywollaars@kpnmail.nl>
2016-10-25 09:43:57 +0200

    AI

---
+++ Scripts/AI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LearningAnimals
+{
+    public class AI : MonoBehaviour
+    {
+        private int m_Joy = 0;
+
+        [SerializeField]
+        private int m_MoveSpeed = 700;
+        [SerializeField]
+        private int m_JumpForce = 50;
+
+        private GameObject m_Target;
+
+        private Rigidbody2D m_Rigidbody;
+
+        void Start()
+        {
+            m_Target = GameObject.Find("Toy");
+            m_Rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            if (Vector2.Distance(transform.position, m_Target.transform.position) > 2)
+            {
+                if (m_Target.transform.position.x > transform.position.x)
+                {
+                    m_Rigidbody.AddForce(new Vector2(1, 0) * m_MoveSpeed * Time.deltaTime, ForceMode2D.Force);
+                }
+                else if (m_Target.transform.position.x < transform.position.x)
+                {
+                    m_Rigidbody.AddForce(new Vector2(1, 0) * -m_MoveSpeed * Time.deltaTime, ForceMode2D.Force);
+                }
+                m_Joy++;
+            }
+
+            if(m_Joy >= 100)
+            {
+                m_Rigidbody.AddForce(new Vector2(0, 1) * m_JumpForce * Time.deltaTime, ForceMode2D.Impulse);
+                m_Joy = 0;
+            }
+        }
+    }
+}
--- Scripts/AnimalController.cs
+++ Scripts/AnimalController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 namespace LearningAnimals
@@ -8,14 +9,22 @@ namespace LearningAnimals
         private GameObject m_Animal;
         private Vector3 m_Position = new Vector3(0f, 0f, 0f);
 
+        private SpriteRenderer m_SR;
+
         [SerializeField]
         private UIManager m_UIManager;
 
         public void ChooseAnimal(GameObject GO)
         {
-            m_Animal = GO;
+            m_Animal = new GameObject();
+            m_Animal.AddComponent<Transform>();
+            m_SR = m_Animal.AddComponent<SpriteRenderer>();
+            m_SR.sprite = GO.GetComponent<Image>().sprite;
 
             GameObject spawnedAnimal = (GameObject)Instantiate(m_Animal, m_Position, Quaternion.identity);
+            spawnedAnimal.AddComponent<BoxCollider2D>();
+            spawnedAnimal.AddComponent<Rigidbody2D>();
+            spawnedAnimal.AddComponent<AI>();
 
             m_UIManager.GetHUD[0].SetActive(false);
         }
